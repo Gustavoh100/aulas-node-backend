b1d75e5bacaa373cb7aa3e428d95b668539767f4
Gustavo Cabral <gustavosila010304@gmail.com>
2026-07-02 22:12:48 -0300

    terminei a parte de subtrair

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import express from 'express'
 const app = express()
 //configurar uma rota para metodo http get na raiz ("/") do aplictivo.
   // paht params - router 
-app.get("/api/v1/somar", (req, res) => {
+app.get("/api/v1/subtrair", (req, res) => {
 //query params 
     const num1 = Number (req.query.num1);
     const num2 = Number (req.query.num2);
-    const resultado = num1 + num2 
+    const resultado = num1 - num2 
 // retorna uma resposta JSON contendo uma mensage "hello world!"
   res.send({ resultado })
 }); // inicia o servidor express na porta 3000 e define uma função de retorno de chamada.
