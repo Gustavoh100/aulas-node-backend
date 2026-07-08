27ad07c36fad0fbb3aafeefc5caf5193abd24480
Gustavo Cabral <gustavosila010304@gmail.com>
2026-07-08 20:55:14 -0300

    terminei as calculadora

--- index.js
+++ index.js
@@ -1,56 +1,43 @@
-// hello.js
+
 import express from 'express'
-import bancoDeDados from './repository/index.js';
 
-// server ou app 
+
 const app = express()
-//configurar uma rota para metodo http get na raiz ("/") do aplictivo.
-  // paht params - router 
-// app.get("/api/v1/subtrair", (req, res) => {
-// //query params 
-//     const num1 = Number (req.query.num1);
-//     const num2 = Number (req.query.num2);
-//     const resultado = num1 - num2 
-// // retorna uma resposta JSON contendo uma mensage "hello world!"
-//   res.send({ resultado })
-// }); 
-
-// HTTP -  POST  ,   GET    , PUT ,  DELETE 
-//CRUD - CREATE ,  READ ,  UPDATE AND DELETE 
-// buscar 
-app.get("/api/pessoa/:id", (req, res) => {
-  const id = req.params.id
-  const pessoa =  bancoDeDados.find( it => it.id == id)
-  if(!pessoa){
-  res.send({  message: "pessoa não encontrada "})
-  return 
-}
- res.send({ pessoa})
-});
-// criar 
-app.get("/api/pessoa" ,(req , res) => {
- const { id  , name  } = req.query
- if(!id || !name){
- res.send({  message: "favor informar um id e um name"})
- return 
-}
- bancoDeDados.push({id , name })
- console.log(bancoDeDados)
- res.send({ message: " pessoa criada com sucesso " })
-});
-//alterar 
-app.get("/api/Delete"  , (req , res ) => {
-const id = req.params.id;
-
-const pessoa = bancoDeDados.find(it => it.id == id );
-
-
-
-});
-
-// inicia o servidor express na porta 3000 e define uma função de retorno de chamada.
+// somar 
+app.get ("/api/v1/somar", (req, res ) => {
+const { num1 , num2 } = req.query 
+const resultado = Number(num1) + Number(num2)
+// if else 
+res.send({
+    message : resultado 
+  })
+})
+app.get ("/api/v1/subtrair", (req, res ) => {
+const { num1 , num2 } = req.query 
+const resultado = Number(num1) - Number(num2)
+// if else 
+res.send({
+    message : resultado 
+  })
+})
+app.get ("/api/v1/dividir", (req, res ) => {
+const { num1 , num2 } = req.query 
+const resultado = Number(num1) / Number(num2)
+// if else 
+res.send({
+    message : resultado 
+  })
+})
+app.get ("/api/v1/mutiplicar", (req, res ) => {
+const { num1 , num2 } = req.query 
+const resultado = Number(num1) * Number(num2)
+// if else 
+res.send({
+    message : resultado 
+  })
+})
 app.listen(3000, () => {
-    //exibe uma mensagem no console quando o servidor estiver ativo. 
+    
     console.log("servidor rodando  na porta 3000")
 });
 
